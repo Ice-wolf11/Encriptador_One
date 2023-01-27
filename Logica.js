91396ec228b35774ab487f69682e31804eaf72eb
Andrew <icewolf2227@gmail.com>
2023-01-26 21:33:44 -0500

    agregando funcionciones copiar, obtener e imprimir vinculandolas con sus recpectivos botones

--- Logica.js
+++ Logica.js
@@ -1,4 +1,36 @@
-function ocultar() {
-    document.getElementById("parrafos").style.display = "none";
-    document.getElementById("copiar").style.display = "initial"
+  function copiar(){
+    //con estp estamos obteniendo el texto del text area, sleccionando el contenido y pasando este al portapapeles
+    var output = document.getElementById("text_out");
+    output.select();
+    output=document.execCommand("copy");//para copiar texto al portapaples
+    alert("Copiado al portapapeles")
+  }
+  
+  function ocultar_mostrar(){
+    document.getElementById("parrafos").style.display = "none";//para ocultar el texto
+    document.getElementById("copiar").style.display = "initial";//para mostrar el texto
+  }
+
+  //funcion para obtener el texto del input
+  function obtener(){
+    var input = document.getElementById("text_in_1").value;
+    return input;
+  }
+
+  //funcion para imprimir el texto obtenido del input en el output
+  function imprimir(){
+    var output = document.getElementById("text_out").textContent = obtener();
+  }
+
+  function encubrir() {
+    ocultar_mostrar();
+    imprimir();
+    
+    
+  
+  }
+
+  function desifrar() {
+    ocultar_mostrar();
+    imprimir();
   }
