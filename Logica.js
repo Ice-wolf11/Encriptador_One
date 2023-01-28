ce66d2576496d9af4aa48f48ba9e227b2da6eff4
Andrew <icewolf2227@gmail.com>
2023-01-27 20:27:13 -0500

    agregando las funciones para detectar mayusculas y acentos

--- Logica.js
+++ Logica.js
@@ -22,25 +22,65 @@
     var output = document.getElementById("text_out").textContent = cadena;
   }
 
-  function encubrir() {
-    ocultar_mostrar();
-    var cadena = obtener();
-    cadena = cadena.replaceAll("e","enter");
-    cadena = cadena.replaceAll("i","imes");
-    cadena = cadena.replaceAll("a","ai");
-    cadena = cadena.replaceAll("o","ober");
-    cadena = cadena.replaceAll("u","ufat");
-    imprimir(cadena);
+  //funcion para detectar mayusculas
+  function Detectarmayus(cadena){
+    var cadena=obtener();
+    for (var x =0;x<cadena.length;x++){
+      var letra = cadena[x];
+        if (letra===letra.toUpperCase()){
+          return true;
+        }else{
+          return false;
+        }
+    }
+  }
   
+  //funcion para detectar acentos
+  function Detectaracento(cadena){
+    var respuesta = false;
+    const mayus=["á","Á","é","É","í","Í","ó","Ó","ú","Ú"];
+    for (var x =0;x<cadena.length;x++){
+      var letra = cadena[x];
+      for (var y =0;y<10;y++){
+        if (letra == mayus[y]){
+          //console.log(letra);
+          respuesta = true;
+        }
+      }
+    }
+    return respuesta;
   }
 
-  function desifrar() {
-    ocultar_mostrar();
+
+  //funcion para encriptar
+  function encubrir() {
     var cadena = obtener();
-    cadena = cadena.replaceAll("enter","e");
-    cadena = cadena.replaceAll("imes","i");
-    cadena = cadena.replaceAll("ai","a");
-    cadena = cadena.replaceAll("ober","o");
-    cadena = cadena.replaceAll("ufat","u");
-    imprimir(cadena);
+    //Detectaracento(cadena);
+    if ( Detectaracento(cadena)||Detectarmayus(cadena)){
+      alert("Solo minusculas y sin acentos")
+    }else{
+      ocultar_mostrar();
+      cadena = cadena.replaceAll("e","enter");
+      cadena = cadena.replaceAll("i","imes");
+      cadena = cadena.replaceAll("a","ai");
+      cadena = cadena.replaceAll("o","ober");
+      cadena = cadena.replaceAll("u","ufat");
+      imprimir(cadena);
+    }
+  }
+//funcion para desencriptar
+  function desifrar() {
+    var cadena = obtener();
+    //Detectaracento(cadena);
+    if ( Detectaracento(cadena)||Detectarmayus(cadena)){
+      alert("Solo minusculas y sin acentos")
+    }else{
+      ocultar_mostrar();
+      cadena = cadena.replaceAll("enter","e");
+      cadena = cadena.replaceAll("imes","i");
+      cadena = cadena.replaceAll("ai","a");
+      cadena = cadena.replaceAll("ober","o");
+      cadena = cadena.replaceAll("ufat","u");
+      imprimir(cadena);
+    }
   }
