7d589d60a8c1ac64f24c6c1b0bb50165cb4ad03b
Andrew <icewolf2227@gmail.com>
2023-02-05 18:25:22 -0500

    agregando la funcion para reaparecer los elementos con el boton copiar

--- Logica.js
+++ Logica.js
@@ -3,9 +3,17 @@
     var output = document.getElementById("text_out");
     output.select();
     output=document.execCommand("copy");//para copiar texto al portapaples
-    alert("Copiado al portapapeles")
+    reaparecer();
+
   }
   
+  function reaparecer(){
+        //con esto borramos el contenido del output
+    document.getElementById("text_out").textContent = "";
+    document.getElementById("parrafos").style.display = "initial";
+    document.getElementById("copiar").style.display = "none";
+  }
+
   function ocultar_mostrar(){
     document.getElementById("parrafos").style.display = "none";//para ocultar el texto
     document.getElementById("copiar").style.display = "initial";//para mostrar el texto
@@ -14,29 +22,40 @@
   //funcion para obtener el texto del input
   function obtener(){
     var input = document.getElementById("text_in_1").value;
-    input = input.toLowerCase();
+    //input = input.toLowerCase();
     return input;
   }
 
   //funcion para imprimir el texto obtenido del input en el output
   function imprimir(cadena){
-    var output = document.getElementById("text_out").textContent = cadena;
+    document.getElementById("text_out").textContent = cadena;
   }
 
   //funcion para detectar mayusculas
-  /*function Detectarmayus(cadena){
+  function Detectarmayus(cadena){
     var respuesta = false;
     const mayus = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","Ñ","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
     for (var x =0;x<cadena.length;x++){
       for (var y = 0;y<mayus.length;y++){
         if (cadena[x]== mayus[y]){
+          console.log("hoal");
           respuesta = true;
         }
       }
     }
     return respuesta;
-  }*/
+  }
   
+  //funcion para detectar mayusculas con menos codigo :v
+  /*function Detectarmayus(cadena){
+    var respuesta = false;
+    cadena = obtener();
+    for (var x = 0;x<cadena.length;x++){
+      
+    }
+    return respuesta;
+  }*/
+
   //funcion para detectar acentos
   function Detectaracento(cadena){
     var respuesta = false;
@@ -58,31 +77,35 @@
   function encubrir() {
     var cadena = obtener();
     //Detectaracento(cadena);
-    if ( Detectaracento(cadena)){
-      alert("Sin acentos")
+    if ( Detectaracento(cadena) || Detectarmayus(cadena)){
+      alert("Sin acentos ni Mayusculas")
     }else{
       ocultar_mostrar();
-      cadena = cadena.replaceAll("e","enter");
-      cadena = cadena.replaceAll("i","imes");
-      cadena = cadena.replaceAll("a","ai");
-      cadena = cadena.replaceAll("o","ober");
-      cadena = cadena.replaceAll("u","ufat");
+      const cambio = ["enter","imes","ai","ober","ufat"];
+      const actual = ["e","i","a","o","u"];
+      for(var x = 0;x<cambio.length;x++){
+        cadena = cadena.replaceAll(actual[x],cambio[x]); 
+      }
       imprimir(cadena);
     }
   }
+
 //funcion para desencriptar
   function desifrar() {
     var cadena = obtener();
     //Detectaracento(cadena);
-    if ( Detectaracento(cadena)){
-      alert("Sin acentos")
+    if ( Detectaracento(cadena) || Detectarmayus(cadena)){
+      alert("Sin acentos ni Mayusculas")
     }else{
       ocultar_mostrar();
-      cadena = cadena.replaceAll("enter","e");
-      cadena = cadena.replaceAll("imes","i");
-      cadena = cadena.replaceAll("ai","a");
-      cadena = cadena.replaceAll("ober","o");
-      cadena = cadena.replaceAll("ufat","u");
+      const cambio = ["enter","imes","ai","ober","ufat"];
+      const actual = ["e","i","a","o","u"];
+      for(var x = 0;x<cambio.length;x++){
+        cadena = cadena.replaceAll(cambio[x],actual[x]); 
+      }
       imprimir(cadena);
     }
   }
+
+
+
